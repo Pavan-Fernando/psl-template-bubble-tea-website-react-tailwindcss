aae9628beb87c3a8ff394ce7f4e569173e096be0
Pavan Vihanga Fernando <59635423+Pavan-Fernando@users.noreply.github.com>
2024-07-11 13:11:48 +0530

    Integrated the tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-    
-    </>
+    <div className="bg-red-600">
+        App
+    </div>
   )
 }
 
