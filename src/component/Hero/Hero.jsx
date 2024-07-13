5a8a484d99a448fada4fa28803ae20794e062af6
Pavan Vihanga Fernando <59635423+Pavan-Fernando@users.noreply.github.com>
2024-07-13 23:59:04 +0530

    annimation adding started

--- src/component/Hero/Hero.jsx
+++ src/component/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import BgImage from '../../assets/bg.png';
 import MainImage from '../../assets/main_image2.png';
 import NavBar from '../NavBar/NavBar';
+import { motion } from "framer-motion";
 
 
 const bgImage = {
@@ -13,44 +14,80 @@ const bgImage = {
 
 
 const Hero = () => {
-  return (
-    <main style={bgImage}>
-        <section className="min-h-[750px] w-full" > 
-            <div className="container">
-                {/* Nav Bar section */}
-                <NavBar />
-                {/* Hero section */}
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-col-3 place-items-center min-h-[850px]">
-                    {/* text content section */}
-                    <div className="text-lightOrange mt-[100px] md:mt-0 p-4 space-y-28 max-w-[475px]">
-                        <h1 className="text-7xl font-bold leading-tight ml-14">PSL Bubble Tea</h1>
-                        <div className="relative">
-                            <div className="relative z-10 space-y-4">
-                                <h1 className="text-2xl">Bubble Tea With PSL,</h1>
-                                <h1 className="text-sm opacity-55 leading-loose">
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                                    Quis porro eius, consequuntur id a laboriosam quae harum corporis. 
-                                    Harum eius et repellat libero dignissimos nesciunt ea temporibus quasi dolor! Harum.
-                                </h1>
-                            </div>
-                            <div className="absolute -top-6 -left-4 w-[250px] h-[200px] bg-gray-700/25"></div>
+    return (
+        <main style={bgImage}>
+            <section className="relative min-h-[750px] w-full" >
+                <div className="container">
+                    {/* Nav Bar section */}
+                    <NavBar />
+                    {/* Hero section */}
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-col-3 place-items-center min-h-[850px]">
+                        {/* text content section */}
+                        <div className="text-lightOrange mt-[100px] md:mt-0 p-4 space-y-28 max-w-[475px]">
+                            <motion.h1
+                                initial={{ opacity: 0, y: -100 }}
+                                animate={{ opacity: 1, y: 0 }}
+                                transition={{
+                                    type: "spring",
+                                    stiffness: 100,
+                                    damping: 10,
+                                    delay: 1
+                                }}
+                                className="text-7xl font-bold leading-tight ml-14">PSL Bubble Tea</motion.h1>
+                            <motion.div
+                                initial={{ opacity: 0, y: -100 }}
+                                animate={{ opacity: 1, y: 0 }}
+                                transition={{
+                                    type: "spring",
+                                    stiffness: 100,
+                                    damping: 10,
+                                    delay: 1.2
+                                }}
+                                className="relative">
+                                <div className="relative z-10 space-y-4">
+                                    <h1 className="text-2xl">Bubble Tea With PSL,</h1>
+                                    <h1 className="text-sm opacity-55 leading-loose">
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                        Quis porro eius, consequuntur id a laboriosam quae harum corporis.
+                                        Harum eius et repellat libero dignissimos nesciunt ea temporibus quasi dolor! Harum.
+                                    </h1>
+                                </div>
+                                <div className="absolute -top-6 -left-4 w-[250px] h-[200px] bg-gray-700/25"></div>
+                            </motion.div>
                         </div>
-                    </div>
-                    {/* Main Image Section */}
-                    <div  className="relative">
-                        <img src={MainImage} alt="main image" className='relative z-40 h-[800px] md:h-[700px] img-shadow left-[50px]'/>
-                        {/* <div className="h-[180px] w-[180px] absolute top-24 right-20  border-primary border-[20px] rounded-full z-10"></div> */}
-                        <div className='absolute top-[350px]  z-[1]'>
-                            <h1 className='text-[40px] scale-100 font-bold text-white/10 leading-none w-[1px]'>Prime Stack Labs Bubble Shop</h1>
+                        {/* Main Image Section */}
+                        <div className="relative">
+                            <motion.img
+                                initial={{ opacity: 0, scale: 0 }}
+                                animate={{ opacity: 1, scale: 1 }}
+                                transition={{
+                                    type: "spring",
+                                    stiffness: 100,
+                                    damping: 10,
+                                    delay: 0.4
+                                }}
+                                src={MainImage} alt="main image" className='relative z-40 h-[800px] md:h-[700px] img-shadow left-[55px]' />
+                            {/* <div className="h-[180px] w-[180px] absolute top-24 right-20  border-primary border-[20px] rounded-full z-10"></div> */}
+                            <motion.div
+                                initial={{ opacity: 0, y: -100 }}
+                                animate={{ opacity: 1, y: 0 }}
+                                transition={{
+                                    type: "spring",
+                                    stiffness: 100,
+                                    damping: 10,
+                                    delay: 1.2
+                                }}
+                                className='absolute top-[350px]  z-[1]'>
+                                <h1 className='text-[40px] scale-100 font-bold text-lightOrange/40 leading-none w-[1px]'>Prime Stack Labs Bubble Shop</h1>
+                            </motion.div>
                         </div>
                     </div>
                 </div>
+                {/* sider menue section */}
                 
-            </div>
-            
-        </section>
-    </main>
-  );
+            </section>
+        </main>
+    );
 };
 
 export default Hero;
--- src/component/NavBar/NavBar.jsx
+++ src/component/NavBar/NavBar.jsx
@@ -1,11 +1,46 @@
 import React from 'react'
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { motion } from "framer-motion";
+// import Logo from '../../assets/logo/logo.jpg';
 
 const NavBar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <nav className='absolute top-0 left-0 w-full pt-10 text-white z-40'>
+            <div className='container'>
+                <div className='flex justify-between items-center'>
+                    {/* logo section */}
+                    {/* <img src={Logo} alt="main-logo" className='max-w-[50px] max-h-[50px]'/> */}
+                    <motion.h1
+                        initial={{ opacity: 0, y: -100 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{
+                            type: "spring",
+                            stiffness: 100,
+                            damping: 10,
+                            delay: 0.2
+                        }}
+                        className='text-2xl font-semibold uppercase'>
+                        <span className='text-primary'>PSL </span>
+                        BUBBLE
+                        <span className='text-primary'> TEA </span>
+                    </motion.h1>
+                    {/* menu section */}
+                    <motion.div
+                        initial={{ opacity: 0, y: -100 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{
+                            type: "spring",
+                            stiffness: 100,
+                            damping: 10,
+                            delay: 0.2
+                        }}>
+                        <GiHamburgerMenu className='text-3xl cursor-pointer'></GiHamburgerMenu>
+                    </motion.div>
+                </div>
+
+            </div>
+        </nav>
+    )
 }
 
 export default NavBar
